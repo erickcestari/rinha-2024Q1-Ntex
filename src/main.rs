1505d09aa74d3801736b3be8bc9032141cb84d51
Erick Cestari <erickcestari03@gmail.com>
2024-02-10 16:10:01 -0300

    feat: add create_transaction route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod services;
+
+use ntex::web;
+use services::create_transaction::create_transaction;
+
+#[ntex::main]
+async fn main() -> std::io::Result<()> {
+    web::HttpServer::new(|| web::App::new().service(create_transaction))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
